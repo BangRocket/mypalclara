c7c1e21d72cb1d26382f640dc4a8732686c5ab47
Codesmith <heidornj@gmail.com>
2026-02-04 15:23:17 -0500

    Merge migration branches [skip-version]

---
+++ db/migrations/versions/b040b33ec24e_merge_migration_branches.py
@@ -0,0 +1,28 @@
+"""merge migration branches
+
+Revision ID: b040b33ec24e
+Revises: 4502328fd583, b2c3d4e5f6g7
+Create Date: 2026-02-04 15:22:25.584789
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b040b33ec24e'
+down_revision: Union[str, Sequence[str], None] = ('4502328fd583', 'b2c3d4e5f6g7')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
